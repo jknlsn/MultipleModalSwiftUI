bfb78d3c692e6970bb0fdc6293b797be19b9bc0e
Jake Nelson <j.n@live.com.au>
2020-06-27 16:24:44 +0800

    Initial commit

--- MultipleModalSwiftUI/ContentView.swift
+++ MultipleModalSwiftUI/ContentView.swift
@@ -9,7 +9,185 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        TabView {
+            FirstView()
+                .tabItem {
+                    Image(systemName: "1.square.fill")
+                    Text("First")
+                }
+            
+            SecondView()
+                .tabItem {
+                    Image(systemName: "2.square.fill")
+                    Text("Second")
+                }
+            
+            ThirdView()
+                .tabItem {
+                    Image(systemName: "3.square.fill")
+                    Text("Third")
+                }
+            
+            FourthView()
+                .tabItem {
+                    Image(systemName: "4.square.fill")
+                    Text("Fourth")
+                }
+        }
+        .font(.headline)
+    }
+}
+
+struct FirstView: View {
+    
+    @State var showModal = false
+    @State var whichModal = ""
+    
+    var body: some View {
+        VStack{
+            Text("switch statement and State initially set to ''")
+            Button(action: {
+                whichModal = "first"
+                self.showModal.toggle()
+            }) {
+                Text("First button")
+            }
+            
+            Button(action: {
+                whichModal = "second"
+                self.showModal.toggle()
+            }) {
+                Text("Second button")
+            }
+        }
+        .sheet(
+            isPresented: $showModal,
+            content: {
+                switch whichModal {
+                case "first":
+                    Text("First modal!")
+                case "second":
+                    Text("Second modal!")
+                default :
+                    Text("Huh?!?")
+                }
+            }
+        )
+    }
+}
+
+struct SecondView: View {
+    
+    @State var showModal = false
+    @State var whichModal = "first"
+    
+    var body: some View {
+        VStack{
+            Text("switch statement and State initially set to 'first'")
+            Button(action: {
+                whichModal = "first"
+                self.showModal.toggle()
+            }) {
+                Text("First button")
+            }
+            Button(action: {
+                whichModal = "second"
+                self.showModal.toggle()
+            }) {
+                Text("Second button")
+            }
+        }
+        .sheet(
+            isPresented: $showModal,
+            content: {
+                switch whichModal {
+                case "first":
+                    Text("First modal!")
+                case "second":
+                    Text("Second modal!")
+                default :
+                    Text("Huh?!?")
+                }
+            }
+        )
+    }
+}
+
+struct ThirdView: View {
+    
+    @State var showModal = false
+    @State var whichModal = "first"
+    
+    var body: some View {
+        VStack{
+            Text("if statement and State initially set to 'first'")
+            Button(action: {
+                whichModal = "first"
+                self.showModal.toggle()
+            }) {
+                Text("First button")
+            }
+            Button(action: {
+                whichModal = "second"
+                self.showModal.toggle()
+            }) {
+                Text("Second button")
+            }
+        }
+        .sheet(
+            isPresented: $showModal,
+            content: {
+                if whichModal == "first" {
+                    Text("First modal!")
+                }
+                else if whichModal == "second" {
+                    Text("Second modal!")
+                }
+                else {
+                    Text("Huh?!?")
+                }
+            }
+        )
+    }
+}
+
+struct FourthView: View {
+    
+    @State var showModal = false
+    @State var whichModal = ""
+    
+    var body: some View {
+        VStack{
+            Text("if statement and State initially set to '', set twice on button press")
+            Button(action: {
+                whichModal = "second"
+                whichModal = "first"
+                self.showModal.toggle()
+            }) {
+                Text("First button")
+            }
+            Button(action: {
+                whichModal = "first"
+                whichModal = "second"
+                self.showModal.toggle()
+            }) {
+                Text("Second button")
+            }
+        }
+        .sheet(
+            isPresented: $showModal,
+            content: {
+                if whichModal == "first" {
+                    Text("First modal!")
+                }
+                else if whichModal == "second" {
+                    Text("Second modal!")
+                }
+                else {
+                    Text("Huh?!?")
+                }
+            }
+        )
     }
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-#  <#Title#>
+#  Multiple Modals in SwiftUI issue in iOS 14
 
+## Issue
+Previously it was possible to have multiple modals effectively achieved in a single sheet view, by showing a different content depending on a `@State` variable. See example below. This is no longer working in SwiftUI in iOS 14, however did work in iOS 13.
+
+When running this example, you should see a modal saying "First view!" or "Second view!" depending on the button pressed, which updates the `@State` variable `whichModal` and then toggles the `showModal` boolean.
+Instead my experience has been that clicking on a button the first time will almost seem as if it has not changed the State variable, then subsequent times work.
+
+Is this a bug, or is there a different way to now achieve this effect?
+
+```swift
+.sheet(
+    isPresented: $showModal,
+    content: {
+        if whichModal == "first" {
+            Text("First modal!")
+        }
+        else if whichModal == "second" {
+            Text("Second modal!")
+        }
+        else {
+            Text("Huh?!?")
+        }
+    }
+)
+```
