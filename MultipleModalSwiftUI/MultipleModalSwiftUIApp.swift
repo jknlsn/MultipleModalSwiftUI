3e24badb76ba17342bfe744008c17a714e6f1305
Jake Nelson <j.n@live.com.au>
2020-06-27 13:56:22 +0800

    Initial Commit

---
+++ MultipleModalSwiftUI/MultipleModalSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleModalSwiftUIApp.swift
+//  MultipleModalSwiftUI
+//
+//  Created by Jake Nelson on 27/6/20.
+//
+
+import SwiftUI
+
+@main
+struct MultipleModalSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
